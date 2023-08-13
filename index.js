2de1bd9620ad589f76aa72dadd5905489cd8458a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-08-13 03:46:16 -0700

    sign in view

--- index.js
+++ index.js
@@ -4,34 +4,45 @@ import { fileURLToPath } from "url";
 
 const router = express.Router();
 
-router.get("/", function(_, res) {
-    res.render("index");
+// views
+router.get("/", function (_, res) {
+  res.render("index");
 });
 
-router.get("/about", function(_, res) {
-    res.render("about", {
-        location: "ur mom",
-    });
+router.get("/about", function (_, res) {
+  res.render("about", {
+    location: "ur mom",
+  });
 });
 
-router.post("/lol", async function(_, res) {
-    res.send(await ejs.renderFile("./hx/lol.ejs", {
-        message: "xd",
-    }));
+router.get("/sign-in", function (_, res) {
+  res.render("sign-in");
+});
+
+// ajax
+router.post("/lol", async function (_, res) {
+  res.send(
+    await ejs.renderFile("./hx/lol.ejs", {
+      message: "xd",
+    })
+  );
 });
 
 async function main() {
-    const app = express();
-    app.set("view engine", "ejs");
+  const app = express();
+  app.set("view engine", "ejs");
 
-    app.use(express.static(fileURLToPath(new URL("./node_modules/htmx.org/dist", import.meta.url))));
-    app.use(express.static(fileURLToPath(new URL("./public", import.meta.url))));
-    // console.log(fileURLToPath(new URL("./public", import.meta.url)));
+  app.use(
+    express.static(
+      fileURLToPath(new URL("./node_modules/htmx.org/dist", import.meta.url))
+    )
+  );
+  app.use(express.static(fileURLToPath(new URL("./public", import.meta.url))));
+  // console.log(fileURLToPath(new URL("./public", import.meta.url)));
 
-    // app.use(express.json());
-    app.use("/", router);
-    app.listen(3000); // todo: hardcoded
+  // app.use(express.json());
+  app.use("/", router);
+  app.listen(3000); // todo: hardcoded
 }
 
 main();
-
