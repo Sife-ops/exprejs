1c5a379876bc2a80c1ebe3b8b6fbcb74a57f6dbe
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-08-15 03:47:52 -0700

    cookies

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import ejs from "ejs";
 import express from "express";
 import { fileURLToPath } from "url";
@@ -5,27 +6,33 @@ import { fileURLToPath } from "url";
 const router = express.Router();
 
 // views
-router.get("/", function (_, res) {
-  res.render("index");
+router.get("/", function(_, res) {
+    res.render("index");
 });
 
-router.get("/about", function (_, res) {
-  res.render("about", {
-    location: "ur mom",
-  });
+router.get("/about", function(_, res) {
+    res.render("about", {
+        location: "ur mom",
+    });
 });
 
-router.get("/sign-in", function (_, res) {
-  res.render("sign-in");
+router.get("/sign-in", function(_, res) {
+    res.render("sign-in");
 });
 
-router.get("/temp", function (_, res) {
-  res.render("temp");
+router.get("/temp", function(_, res) {
+    res.render("temp");
 });
 
 // ajax
-router.get("/temp_", function (_, res) {
-    res.send(`
+router.get("/temp_", function(_, res) {
+    res
+        .cookie("tk", "todo", {
+            httpOnly: true,
+            signed: true,
+            // secure: true,
+        })
+        .send(`
         <div>
             <div>swap elem1</div>
         </div>
@@ -37,46 +44,48 @@ router.get("/temp_", function (_, res) {
     `);
 });
 
-router.post("/lol", async function (_, res) {
-  res.send(
-    await ejs.renderFile("./hx/lol.ejs", {
-      message: "xd",
-    })
-  );
+router.post("/lol", async function(req, res) {
+    console.log(req.signedCookies);
+    res.send(
+        await ejs.renderFile("./hx/lol.ejs", {
+            message: "xd",
+        })
+    );
 });
 
-router.post("/sign-in_", async function (req, res) {
-  const fakeEmail = "email1";
-  const fakePass = "pass1";
-  if (req.body.email !== fakeEmail || req.body.password !== fakePass) {
-    return res.send(await ejs.renderFile("./hx/sign-in-problem.ejs"));
-  }
-  res
-    .setHeader(
-      "HX-Trigger",
-      '{"save-token": {"token": "todo", "other": "something"}}'
-    )
-    .setHeader("HX-Redirect", "/about")
-    .send("");
+router.post("/sign-in_", async function(req, res) {
+    const fakeEmail = "email1";
+    const fakePass = "pass1";
+    if (req.body.email !== fakeEmail || req.body.password !== fakePass) {
+        return res.send(await ejs.renderFile("./hx/sign-in-problem.ejs"));
+    }
+    res
+        .setHeader(
+            "HX-Trigger",
+            '{"save-token": {"token": "todo", "other": "something"}}'
+        )
+        .setHeader("HX-Redirect", "/about")
+        .send("");
 });
 
 // server
 async function main() {
-  const app = express();
-  app.set("view engine", "ejs");
+    const app = express();
+    app.set("view engine", "ejs");
 
-  //   app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+    //   app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser("todo: secret"));
 
-  app.use(
-    express.static(
-      fileURLToPath(new URL("./node_modules/htmx.org/dist", import.meta.url))
-    )
-  );
-  app.use(express.static(fileURLToPath(new URL("./public", import.meta.url))));
+    app.use(
+        express.static(
+            fileURLToPath(new URL("./node_modules/htmx.org/dist", import.meta.url))
+        )
+    );
+    app.use(express.static(fileURLToPath(new URL("./public", import.meta.url))));
 
-  app.use("/", router);
-  app.listen(3001); // todo: hardcoded
+    app.use("/", router);
+    app.listen(3001); // todo: hardcoded
 }
 
 main();
